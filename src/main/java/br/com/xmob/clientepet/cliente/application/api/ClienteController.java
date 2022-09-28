44b49e19b1a3a3a8671f43f78599a901aca4eae0
Oscar-devjava <negociante2t@gmail.com>
2022-09-28 06:46:50 -0300

    implementa fluxo de controller

--- src/main/java/br/com/xmob/clientepet/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/xmob/clientepet/cliente/application/api/ClienteController.java
@@ -9,5 +9,11 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 @RequiredArgsConstructor
 public class ClienteController implements ClienteAPI {
+	@Override
+	public ClienteRespose postCadastraNovoCliente(ClienteRequest clienteRequest) {
+		log.info("[inicia] ClienteController - postCadastraNovoCliente");
+		log.info("[finaliza] ClienteController - postCadastraNovoCliente");
+		return null;
+	}
 
 }
