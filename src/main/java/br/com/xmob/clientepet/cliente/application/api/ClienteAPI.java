63a9a1cafa54a20383dd139673b7f49128b9e078
Oscar-devjava <negociante2t@gmail.com>
2022-09-28 06:42:26 -0300

    implementa camada de api

--- src/main/java/br/com/xmob/clientepet/cliente/application/api/ClienteAPI.java
+++ src/main/java/br/com/xmob/clientepet/cliente/application/api/ClienteAPI.java
@@ -5,4 +5,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/v1/cliente")
 public interface ClienteAPI {
 
+	ClienteRespose postCadastraNovoCliente(ClienteRequest clienteRequest);
 }
