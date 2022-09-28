6c8910d0265f285df560acb67d704682968129dd
Oscar-devjava <negociante2t@gmail.com>
2022-09-28 07:13:10 -0300

    builda cliente

--- src/main/java/br/com/xmob/clientepet/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/xmob/clientepet/cliente/application/service/ClienteApplicationService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.xmob.clientepet.cliente.application.api.ClienteRequest;
 import br.com.xmob.clientepet.cliente.application.api.ClienteRespose;
+import br.com.xmob.clientepet.cliente.application.repository.ClienteRepository;
 import br.com.xmob.clientepet.cliente.domain.Cliente;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -13,12 +14,14 @@ import lombok.extern.log4j.Log4j2;
 @RequiredArgsConstructor
 public class ClienteApplicationService implements ClienteService {
 
+	private final ClienteRepository clienteRepository;
+
 	@Override
 	public ClienteRespose criaNovoCliente(ClienteRequest clienteRequest) {
 		log.info("[inicia] ClienteApplicationService - criaNovoCliente");
-		ClienteRespose clienteCriado = clienteRepository.salva(new Cliente(clienteRequest));
+		Cliente clienteCriado = clienteRepository.salva(new Cliente(clienteRequest));
 		log.info("[inicia] ClienteApplicationService - criaNovoCliente");
-		return clienteCriado;
+		return ClienteRespose.builder().idCliente(clienteCriado.getIdCliente()).build();
 	}
 
 }
