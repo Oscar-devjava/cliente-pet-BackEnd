222dfc20d1723135b5ba73b76194ca506bc2542a
Oscar-devjava <negociante2t@gmail.com>
2022-09-28 07:16:24 -0300

    implementa camda de infra,test ok, retornando id cliente

--- src/main/java/br/com/xmob/clientepet/cliente/application/repository/ClienteRepository.java
+++ src/main/java/br/com/xmob/clientepet/cliente/application/repository/ClienteRepository.java
@@ -1,5 +1,10 @@
 package br.com.xmob.clientepet.cliente.application.repository;
 
+import br.com.xmob.clientepet.cliente.application.api.ClienteRespose;
+import br.com.xmob.clientepet.cliente.domain.Cliente;
+
 public interface ClienteRepository {
+
+	Cliente salva(Cliente cliente);
 
 }
