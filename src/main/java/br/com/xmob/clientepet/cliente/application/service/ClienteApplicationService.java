601b76a2321f376352814eb18acb7b4ff55d27aa
Oscar-devjava <negociante2t@gmail.com>
2022-09-28 06:56:13 -0300

    implementa camada de application

--- src/main/java/br/com/xmob/clientepet/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/xmob/clientepet/cliente/application/service/ClienteApplicationService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.xmob.clientepet.cliente.application.api.ClienteRequest;
 import br.com.xmob.clientepet.cliente.application.api.ClienteRespose;
+import br.com.xmob.clientepet.cliente.domain.Cliente;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -15,8 +16,9 @@ public class ClienteApplicationService implements ClienteService {
 	@Override
 	public ClienteRespose criaNovoCliente(ClienteRequest clienteRequest) {
 		log.info("[inicia] ClienteApplicationService - criaNovoCliente");
+		ClienteRespose clienteCriado = clienteRepository.salva(new Cliente(clienteRequest));
 		log.info("[inicia] ClienteApplicationService - criaNovoCliente");
-		return null;
+		return clienteCriado;
 	}
 
 }
