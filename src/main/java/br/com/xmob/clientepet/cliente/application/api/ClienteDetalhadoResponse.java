1731b85933d48870feb5717e4c05dd638f896cf0
Oscar-devjava <negociante2t@gmail.com>
2022-09-29 06:56:02 -0300

    construtor de cliente em cliente detalhado

--- src/main/java/br/com/xmob/clientepet/cliente/application/api/ClienteDetalhadoResponse.java
+++ src/main/java/br/com/xmob/clientepet/cliente/application/api/ClienteDetalhadoResponse.java
@@ -3,6 +3,7 @@ package br.com.xmob.clientepet.cliente.application.api;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import br.com.xmob.clientepet.cliente.domain.Cliente;
 import br.com.xmob.clientepet.cliente.domain.Sexo;
 import lombok.Getter;
 
@@ -16,4 +17,15 @@ public class ClienteDetalhadoResponse {
 	private String cpf;
 	private LocalDate dataNascimento;
 	private LocalDateTime dataHoraDoCadastro;
+	
+	public ClienteDetalhadoResponse(Cliente cliente) {
+		this.nomeCompleto = cliente.getNomeCompleto();
+		this.email =  cliente.getEmail();
+		this.celular = cliente.getCelular();
+		this.telefone = cliente.getTelefone();
+		this.sexo = cliente.getSexo();
+		this.cpf = cliente.getCpf();
+		this.dataNascimento = cliente.getDataNascimento();
+		this.dataHoraDoCadastro = cliente.getDataHoraDoCadastro();	
+	}
 }
