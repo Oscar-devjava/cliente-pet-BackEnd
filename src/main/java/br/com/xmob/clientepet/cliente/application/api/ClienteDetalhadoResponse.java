9159c90ebc7db7adc213a5718e9e39c1c5160107
Oscar-devjava <negociante2t@gmail.com>
2022-09-29 06:52:46 -0300

    implementa classe de detalhes

--- src/main/java/br/com/xmob/clientepet/cliente/application/api/ClienteDetalhadoResponse.java
+++ src/main/java/br/com/xmob/clientepet/cliente/application/api/ClienteDetalhadoResponse.java
@@ -1,8 +1,19 @@
 package br.com.xmob.clientepet.cliente.application.api;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import br.com.xmob.clientepet.cliente.domain.Sexo;
 import lombok.Getter;
 
 @Getter
 public class ClienteDetalhadoResponse {
-
+	private String nomeCompleto;
+	private String email;
+	private String celular;
+	private String telefone;
+	private Sexo sexo;
+	private String cpf;
+	private LocalDate dataNascimento;
+	private LocalDateTime dataHoraDoCadastro;
 }
