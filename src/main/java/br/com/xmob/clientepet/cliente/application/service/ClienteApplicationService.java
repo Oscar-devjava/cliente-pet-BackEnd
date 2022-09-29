a4aa98592c354c97541d7422031042fc8a5a3047
Oscar-devjava <negociante2t@gmail.com>
2022-09-29 06:51:37 -0300

    implementa camada de servico

--- src/main/java/br/com/xmob/clientepet/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/xmob/clientepet/cliente/application/service/ClienteApplicationService.java
@@ -1,9 +1,11 @@
 package br.com.xmob.clientepet.cliente.application.service;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import br.com.xmob.clientepet.cliente.application.api.ClienteDetalhadoResponse;
 import br.com.xmob.clientepet.cliente.application.api.ClienteListResponse;
 import br.com.xmob.clientepet.cliente.application.api.ClienteRequest;
 import br.com.xmob.clientepet.cliente.application.api.ClienteRespose;
@@ -33,6 +35,14 @@ public class ClienteApplicationService implements ClienteService {
 		List<Cliente> clientes = clienteRepository.buscaTodosClientes();
 		log.info("[finaliza] ClienteApplicationService - listaClientes");
 		return ClienteListResponse.converte(clientes);
+	}
+
+	@Override
+	public ClienteDetalhadoResponse clienteDetalhado(UUID idCliente) {
+		log.info("[inicia] ClienteApplicationService - clienteDetalhado");
+		Cliente cliente = clienteRepository.buscaClientePorId(idCliente);
+		log.info("[finaliza] ClienteApplicationService - clienteDetalhado");
+		return new ClienteDetalhadoResponse(cliente);
 	}
 
 }
--- src/main/java/br/com/xmob/clientepet/cliente/application/service/ClienteService.java
+++ src/main/java/br/com/xmob/clientepet/cliente/application/service/ClienteService.java
@@ -1,7 +1,9 @@
 package br.com.xmob.clientepet.cliente.application.service;
 
 import java.util.List;
+import java.util.UUID;
 
+import br.com.xmob.clientepet.cliente.application.api.ClienteDetalhadoResponse;
 import br.com.xmob.clientepet.cliente.application.api.ClienteListResponse;
 import br.com.xmob.clientepet.cliente.application.api.ClienteRequest;
 import br.com.xmob.clientepet.cliente.application.api.ClienteRespose;
@@ -11,5 +13,7 @@ public interface ClienteService {
 	ClienteRespose criaNovoCliente(ClienteRequest clienteRequest);
 
 	List<ClienteListResponse> listaClientes();
+
+	ClienteDetalhadoResponse clienteDetalhado(UUID idCliente);
 
 }
